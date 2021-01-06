fa1ae33e8cbf337912e591d64d1cd758e9567155
Ildefonso Muro <ildefonso.muro@gmail.com>
2021-01-05 23:29:07 -0600

    Peticiones a la api de rick and morty

--- src/callback/index.js
+++ src/callback/index.js
@@ -1,14 +1,23 @@
 let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+let API = 'https://rickandmortyapi.com/api/character/';
 
-function fetData(url_api, callback) {
+function fetchData(url_api, callback) {
     let xhttp = new XMLHttpRequest();
+    // abrir una conexion con el metodo, la ruta y si es asincrono
     xhttp.open('GET', url_api, true);
-    xhttp.onreadystatechange = function (event) {
-        if(xhttp.readyState === 4) {
-            if(xhttp.status === 200){
+    // validacion del llamado
+    xhttp.onreadystatechange = (event) => {
+        // el state 4 es el ultimo de la peticion
+        if (xhttp.readyState === 4) {
+            // verificamos que el status este en 200, que dice que bien, no un 400 o 500
+            if (xhttp.status === 200) {
+                // el primer valor es el err, y el siguiente el resultado
+                // ejecutamos el callback con el resultado
                 callback(null, JSON.parse(xhttp.responseText));
             } else {
-                const error = new Error('Error ' + url_api);
+                // si no es 200
+                let error = new Error('Error: ' + url_api);
+                // matamos el proceso con un error
                 return callback(error, null);
             }
         }
@@ -16,3 +25,29 @@ function fetData(url_api, callback) {
     xhttp.send();
 }
 
+fetchData(API, function (error1, data1) {
+    if (error1) return console.log(error1)
+
+    fetchData(API + data1.results[0].id, function (error2, data2) {
+        if (error2) return console.log(error2)
+
+        fetchData(data2.origin.url, function (error3, data3) {
+            if (error3) return console.log(error3)
+
+            fetchData(data3.residents[0], function (error4, data4) {
+                if (error4) return console.log(error4)
+
+                console.log(data1.info.count);
+                console.log(data2.name);
+                console.log(data2.species);
+                console.log(data2.gender);
+                console.log(data3.name);
+                console.log(data3.type);
+                console.log(data3.dimension);
+                console.log(data4.name);
+                console.log(data4.species);
+                console.log(data4.gender);
+            })
+        })
+    })
+})
